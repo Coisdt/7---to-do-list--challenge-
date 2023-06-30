e885b84d116a42e1f494a5350f1ecf7b36d23cf0
Francois <cois25@live.com>
2023-07-01 01:22:11 +0200

    js: delete task from list + bug fix: list item dbl

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const listContainer = document.querySelector(".list-container");
 const toDoList = document.querySelector(".to-do-list");
 
 function addItem() {
+  // create new element
   const listItem = document.createElement("li");
+  listItem.setAttribute('class', 'item')
   const inputValue = inputElement.value;
   //   if input empty
   if (inputElement.value.trim() === "") {
@@ -12,14 +14,32 @@ function addItem() {
   }
   //   add item to list
   listItem.innerHTML = `
-  <li class='item'><input class='checkbox' type="checkbox"></input>${inputValue}
+  <input class='checkbox' type="checkbox"></input>${
+    inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
+  }
   <i class="delete fa-solid fa-trash"></i> 
   <i class="edit fa-solid fa-pencil">
   </i>
-  </li>`;
+  `;
   toDoList.appendChild(listItem);
   //   reset input
   inputElement.value = "";
+
+  const editBtn = document.querySelector(".edit");
+
+  // delete item from list
+  const deleteBtn = document.querySelectorAll(".delete");
+  deleteBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const targetListItem = e.target.parentElement;
+      targetListItem.remove()
+    });
+  });
 }
 
 addItemBtn.addEventListener("click", addItem);
+
+
+// edit btn
+
+// cross out completed
