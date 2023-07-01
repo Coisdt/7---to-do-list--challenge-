62005723a2ebeb4aee418142a827ace7798447c4
Francois <cois25@live.com>
2023-07-01 03:21:32 +0200

    js: edit list item + save changes notific + bugfix

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const toDoList = document.querySelector(".to-do-list");
 function addItem() {
   // create new element
   const listItem = document.createElement("li");
-  listItem.setAttribute('class', 'item')
-  const inputValue = inputElement.value;
+  listItem.setAttribute("class", "item");
+  let inputValue = inputElement.value;
   //   if input empty
   if (inputElement.value.trim() === "") {
     return;
@@ -17,29 +17,61 @@ function addItem() {
   <input class='checkbox' type="checkbox"></input>${
     inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
   }
-  <i class="delete fa-solid fa-trash"></i> 
   <i class="edit fa-solid fa-pencil">
   </i>
+  <i class="delete fa-solid fa-trash"></i> 
   `;
   toDoList.appendChild(listItem);
   //   reset input
   inputElement.value = "";
 
-  const editBtn = document.querySelector(".edit");
+  // edit item in list
+  const editBtns = document.querySelectorAll(".edit");
+  editBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const toDoInput = document.querySelector(".to-do-input");
+      const targetListItem = e.target.parentElement;
+      let targetContent = targetListItem.textContent.trim();
+
+      inputElement.value = targetContent;
+      console.log(toDoInput);
+      addItemBtn.textContent = "Save";
+      targetListItem.remove();
+      // save notification
+      notifyChanges();
+    });
+    hideElement();
+    addItemBtn.textContent = "Add";
+  });
 
   // delete item from list
   const deleteBtn = document.querySelectorAll(".delete");
   deleteBtn.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       const targetListItem = e.target.parentElement;
-      targetListItem.remove()
+      targetListItem.remove();
+      // delete notification
+      const saveChanges = document.querySelector(".saved-changes");
+      saveChanges.textContent = "Your task was successfully deleted";
+      hideElement();
     });
   });
 }
 
+// save changes notification
+const saveChanges = document.querySelector(".saved-changes");
+function notifyChanges() {
+  addItemBtn.addEventListener("click", () => {
+    saveChanges.textContent = "Your changes have been saved!";
+  });
+}
+
+function hideElement() {
+  setTimeout(() => {
+    saveChanges.textContent = "";
+  }, 3000);
+}
+
 addItemBtn.addEventListener("click", addItem);
-
-
-// edit btn
 
 // cross out completed
